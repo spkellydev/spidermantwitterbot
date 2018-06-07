66a09cb6e29839cd427e2addbfd42c57fe1a51d4
Sean Kelly <spkellydev@gmail.com>
2018-06-07 14:10:26 -0400

    changing interaction intervals

--- bot.js
+++ bot.js
@@ -151,6 +151,7 @@ retweet();
 retweetA();
 // retweet in every 2 minutes
 setInterval(retweet, 60 * 1000 * 6);
+setInterval(retweetA, 60 * 1000 * 13);
 
 // FAVORITE BOT====================
 
@@ -185,76 +186,10 @@ var favoriteTweet = function() {
   });
 };
 
-// function pick_random_image(images) {
-//   return images[Math.floor(Math.random() * images.length)];
-// }
-
-// function upload_random_image(images) {
-//   console.log("Opening an image...");
-//   var image_path = path.join(__dirname, "/images/" + pick_random_image(images)),
-//     b64content = fs.readFileSync(image_path, { encoding: "base64" });
-
-//   console.log("Uploading an image...");
-
-//   T.post("media/upload", { media_data: b64content }, function(
-//     err,
-//     data,
-//     response
-//   ) {
-//     if (err) {
-//       console.log("ERROR:");
-//       console.log(err);
-//     } else {
-//       console.log("Image uploaded!");
-//       console.log("Now tweeting it...");
-
-//       T.post(
-//         "statuses/update",
-//         {
-//           media_ids: new Array(data.media_id_string)
-//         },
-//         function(err, data, response) {
-//           if (err) {
-//             console.log("ERROR:");
-//             console.log(err);
-//           } else {
-//             console.log("Posted an image!");
-//           }
-//         }
-//       );
-//     }
-//   });
-// }
-
-// fs.readdir(__dirname + '/images', function(err, files) {
-//   if (err){
-//     console.log(err);
-//   }
-//   else{
-//     var images = [];
-//     files.forEach(function(f) {
-//       images.push(f);
-//     });
-
-//   /*
-//     You have two options here. Either you will keep your bot running, and upload images using setInterval (see below; 10000 means '10 milliseconds', or 10 seconds), --
-//   */
-//     setInterval(function(){
-//       upload_random_image(images);
-//     }, 1000*60*4);
-
-//   /*
-//     Or you could use cron (code.tutsplus.com/tutorials/scheduling-tasks-with-cron-jobs--net-8800), in which case you just need:
-//   */
-
-//     // upload_random_image(images);
-//   }
-// });
-
 // grab & 'favorite' as soon as program is running...
 favoriteTweet();
 // 'favorite' a tweet in every 3 minutes
-setInterval(favoriteTweet, 30 * 1000 * 20);
+setInterval(favoriteTweet, 60 * 1000 * 5);
 
 // function to generate a random tweet tweet
 function ranDom(arr) {
